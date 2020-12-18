7ab1fe8d73a12053e43359eeaae5531b2b665639
huangweichang <weichang@bianjie.ai>
2020-12-18 14:34:41 +0800

    update code for msg segment

--- msgs/gov/gov.go
+++ msgs/gov/gov.go
@@ -1,15 +1,14 @@
 package gov
 
 import (
-	. "github.com/bianjieai/irita-sync/msgs"
 	"github.com/bianjieai/irita-sync/models"
-	"encoding/json"
+	. "github.com/bianjieai/irita-sync/msgs"
 )
 
 type DocTxMsgSubmitProposal struct {
 	Proposer       string        `bson:"proposer"`        //  Address of the proposer
 	InitialDeposit []models.Coin `bson:"initial_deposit"` //  Initial deposit paid by sender. Must be strictly positive.
-	Content        string        `bson:"content"`
+	Content        interface{}   `bson:"content"`
 }
 
 func (doctx *DocTxMsgSubmitProposal) GetType() string {
@@ -18,8 +17,7 @@ func (doctx *DocTxMsgSubmitProposal) GetType() string {
 
 func (doctx *DocTxMsgSubmitProposal) BuildMsg(txMsg interface{}) {
 	msg := txMsg.(*MsgSubmitProposal)
-	content, _ := json.Marshal(msg.Content)
-	doctx.Content = string(content)
+	doctx.Content = msg.GetContent()
 	doctx.Proposer = msg.Proposer
 	doctx.InitialDeposit = models.BuildDocCoins(msg.InitialDeposit)
 }
