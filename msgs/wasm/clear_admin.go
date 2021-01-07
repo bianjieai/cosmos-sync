bc70c3594fcf4b7d91f29762f855f4612e00cf8c
huangweichang <weichang@bianjie.ai>
2021-01-07 19:37:50 +0800

    close wasm code

--- msgs/wasm/clear_admin.go
+++ msgs/wasm/clear_admin.go
@@ -1,36 +1,36 @@
 package wasm
-
-import . "github.com/bianjieai/irita-sync/msgs"
-
-// MsgClearAdmin removes any admin stored for a smart contract
-type DocMsgClearAdmin struct {
-	// Sender is the that actor that signed the messages
-	Sender string `bson:"sender"`
-	// Contract is the address of the smart contract
-	Contract string `bson:"contract"`
-}
-
-func (m *DocMsgClearAdmin) GetType() string {
-	return MsgTypeClearAdmin
-}
-
-func (m *DocMsgClearAdmin) BuildMsg(v interface{}) {
-	msg := v.(*MsgClearAdmin)
-	m.Sender = msg.Sender
-	m.Contract = msg.Contract
-}
-
-func (m *DocMsgClearAdmin) HandleTxMsg(v SdkMsg) MsgDocInfo {
-	var (
-		addrs []string
-		msg   MsgClearAdmin
-	)
-
-	ConvertMsg(v, &msg)
-	addrs = append(addrs, msg.Sender, msg.Contract)
-	handler := func() (Msg, []string) {
-		return m, addrs
-	}
-
-	return CreateMsgDocInfo(v, handler)
-}
+//
+//import . "github.com/bianjieai/irita-sync/msgs"
+//
+//// MsgClearAdmin removes any admin stored for a smart contract
+//type DocMsgClearAdmin struct {
+//	// Sender is the that actor that signed the messages
+//	Sender string `bson:"sender"`
+//	// Contract is the address of the smart contract
+//	Contract string `bson:"contract"`
+//}
+//
+//func (m *DocMsgClearAdmin) GetType() string {
+//	return MsgTypeClearAdmin
+//}
+//
+//func (m *DocMsgClearAdmin) BuildMsg(v interface{}) {
+//	msg := v.(*MsgClearAdmin)
+//	m.Sender = msg.Sender
+//	m.Contract = msg.Contract
+//}
+//
+//func (m *DocMsgClearAdmin) HandleTxMsg(v SdkMsg) MsgDocInfo {
+//	var (
+//		addrs []string
+//		msg   MsgClearAdmin
+//	)
+//
+//	ConvertMsg(v, &msg)
+//	addrs = append(addrs, msg.Sender, msg.Contract)
+//	handler := func() (Msg, []string) {
+//		return m, addrs
+//	}
+//
+//	return CreateMsgDocInfo(v, handler)
+//}
