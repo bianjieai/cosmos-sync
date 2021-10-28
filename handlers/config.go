88389913424db7cad7a3ecd243480be14104ef99
huangweichang <weichang@bianjie.ai>
2021-10-28 19:46:47 +0800

    refactor initBech32Prefix

--- handlers/config.go
+++ handlers/config.go
@@ -5,23 +5,14 @@ import (
 )
 
 const (
-	// Bech32ChainPrefix defines the prefix of this chain
-	Bech32ChainPrefix = "i"
-
-	// PrefixAcc is the prefix for account
-	PrefixAcc = "a"
-
 	// PrefixValidator is the prefix for validator keys
-	PrefixValidator = "v"
-
+	PrefixValidator = "val"
 	// PrefixConsensus is the prefix for consensus keys
-	PrefixConsensus = "c"
-
-	// PrefixPublic is the prefix for public
-	PrefixPublic = "p"
-
-	// PrefixAddress is the prefix for address
-	PrefixAddress = "a"
+	PrefixConsensus = "cons"
+	// PrefixPublic is the prefix for public keys
+	PrefixPublic = "pub"
+	// PrefixOperator is the prefix for operator keys
+	PrefixOperator = "oper"
 )
 
 var (
@@ -40,12 +31,17 @@ var (
 )
 
 func initBech32Prefix(bech32AccPrefix string) {
-	Bech32PrefixAccAddr = bech32AccPrefix + PrefixAcc + PrefixAddress
-	Bech32PrefixAccPub = bech32AccPrefix + PrefixAcc + PrefixPublic
-	Bech32PrefixValAddr = bech32AccPrefix + PrefixValidator + PrefixAddress
-	Bech32PrefixValPub = bech32AccPrefix + PrefixValidator + PrefixPublic
-	Bech32PrefixConsAddr = bech32AccPrefix + PrefixConsensus + PrefixAddress
-	Bech32PrefixConsPub = bech32AccPrefix + PrefixConsensus + PrefixPublic
+	Bech32PrefixAccAddr = bech32AccPrefix
+	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+	Bech32PrefixAccPub = Bech32PrefixAccAddr + PrefixPublic
+	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+	Bech32PrefixValAddr = Bech32PrefixAccAddr + PrefixValidator + PrefixOperator
+	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+	Bech32PrefixValPub = Bech32PrefixAccAddr + PrefixValidator + PrefixOperator + PrefixPublic
+	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+	Bech32PrefixConsAddr = Bech32PrefixAccAddr + PrefixValidator + PrefixConsensus
+	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+	Bech32PrefixConsPub = Bech32PrefixAccAddr + PrefixValidator + PrefixConsensus + PrefixPublic
 
 	codec.SetBech32Prefix(Bech32PrefixAccAddr, Bech32PrefixAccPub, Bech32PrefixValAddr,
 		Bech32PrefixValPub, Bech32PrefixConsAddr, Bech32PrefixConsPub)
