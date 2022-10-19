8e2011eac16d10ea8cc0c79e49858445d09e2ded
huangweichang <weichang@bianjie.ai>
2022-10-19 16:11:46 +0800

    add define txDecoder

--- libs/msgparser/codec/codec.go
+++ libs/msgparser/codec/codec.go
@@ -4,16 +4,17 @@ import (
 	okexchaincodec "github.com/okex/exchain/app/codec"
 	"github.com/okex/exchain/libs/cosmos-sdk/codec"
 	_ "github.com/okex/exchain/libs/cosmos-sdk/crypto"
+	ibctx "github.com/okex/exchain/libs/cosmos-sdk/types/ibc-adapter"
 	"github.com/okex/exchain/libs/cosmos-sdk/types/module"
-	okmodule "github.com/okex/exchain/libs/cosmos-sdk/types/module"
 	"github.com/okex/exchain/libs/cosmos-sdk/x/auth"
 	cosmoscryptocodec "github.com/okex/exchain/libs/cosmos-sdk/x/auth/ibc-tx"
 	stdtx "github.com/okex/exchain/libs/cosmos-sdk/x/auth/types"
 )
 
 var (
-	appModules = []okmodule.AppModuleBasic{auth.AppModuleBasic{}}
+	appModules = []module.AppModuleBasic{auth.AppModuleBasic{}}
 	codecProxy *codec.CodecProxy
+	txDecoder  ibctx.IbcTxDecoder
 )
 
 func InitTxDecoder() {
@@ -22,6 +23,7 @@ func InitTxDecoder() {
 	interfaceReg := okexchaincodec.MakeIBC(moduleBasics)
 	protoCodec := codec.NewProtoCodec(interfaceReg)
 	codecProxy = codec.NewCodecProxy(protoCodec, cdc)
+	txDecoder = cosmoscryptocodec.IbcTxDecoder(codecProxy.GetProtocMarshal())
 	return
 }
 
@@ -30,13 +32,13 @@ func GetCodec() *codec.ProtoCodec {
 }
 
 func GetSigningTx(txBytes []byte) (*stdtx.IbcTx, error) {
-	tx, err := cosmoscryptocodec.IbcTxDecoder(codecProxy.GetProtocMarshal())(txBytes)
+	tx, err := txDecoder(txBytes)
 	if err != nil {
 		return nil, err
 	}
 	return tx, nil
 }
 
-func RegisterAppModules(module ...okmodule.AppModuleBasic) {
+func RegisterAppModules(module ...module.AppModuleBasic) {
 	appModules = append(appModules, module...)
 }
