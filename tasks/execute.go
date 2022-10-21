32dd77ba694f4dd638e580ea1e6b150b4689063d
huangweichang <weichang@bianjie.ai>
2022-10-21 15:51:44 +0800

    fix code descript

--- tasks/execute.go
+++ tasks/execute.go
@@ -262,6 +262,7 @@ func (s *syncTaskService) TakeOverTaskAndExecute(task models.SyncTask, healthChe
 }
 
 // assert task is valid
+// valid task status: "underway" or "unhandled"
 // valid catch up task: current_height < end_height
 // valid follow task: current_height + blockNumPerWorkerHandle > blockChainLatestHeight
 func assertTaskValid(task models.SyncTask, blockNumPerWorkerHandle int64) (int64, bool) {
@@ -298,7 +299,7 @@ func assertTaskValid(task models.SyncTask, blockNumPerWorkerHandle int64) (int64
 		}
 		break
 	}
-	//judge task status is "underway" when flag is true
+	//judge task status is "underway" or "unhandled" when flag is true
 	if flag {
 		taskStatusUnderway := task.Status == models.SyncTaskStatusUnderway
 		taskStatusUnhandled := task.Status == models.SyncTaskStatusUnHandled
