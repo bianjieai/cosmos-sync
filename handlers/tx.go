2c7431a5ddb8cacdf6e711be5b565af9cd2ae5f7
yingjian <yingjian@bianjie.ai>
2023-04-21 15:12:04 +0800

    update  code

--- handlers/tx.go
+++ handlers/tx.go
@@ -83,7 +83,6 @@ func ParseBlockAndTxs(b int64, client *pool.Client) (*models.Block, []*models.Tx
 	}
 
 	txDocs := make([]*models.Tx, 0, len(block.Block.Txs))
-
 	blockResults, err := client.BlockResults(context.Background(), &b)
 	if err != nil {
 		time.Sleep(1 * time.Second)
@@ -94,21 +93,21 @@ func ParseBlockAndTxs(b int64, client *pool.Client) (*models.Block, []*models.Tx
 			}
 			return &blockDoc, nil, utils.ConvertErr(b, "", "ParseBlockResult", err)
 		}
+	}
 
-		if len(block.Block.Txs) != len(blockResults.TxsResults) {
-			return nil, nil, utils.ConvertErr(b, "", "block.Txs length not equal blockResult", nil)
-		}
+	if len(block.Block.Txs) != len(blockResults.TxsResults) {
+		return nil, nil, utils.ConvertErr(b, "", "block.Txs length not equal blockResult", nil)
+	}
 
-		if len(block.Block.Txs) > 0 {
-			for i, v := range block.Block.Txs {
-				txResult := blockResults.TxsResults[i]
-				txDoc, err := parseTx(v, txResult, block.Block, uint32(i))
-				if err != nil {
-					return &blockDoc, txDocs, err
-				}
-				if txDoc.TxHash != "" {
-					txDocs = append(txDocs, &txDoc)
-				}
+	if len(block.Block.Txs) > 0 {
+		for i, v := range block.Block.Txs {
+			txResult := blockResults.TxsResults[i]
+			txDoc, err := parseTx(v, txResult, block.Block, uint32(i))
+			if err != nil {
+				return &blockDoc, txDocs, err
+			}
+			if txDoc.TxHash != "" {
+				txDocs = append(txDocs, &txDoc)
 			}
 		}
 	}
